3216733cba20defc094ae146d3bd254294913392
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-03-13 04:23:07 +0000

    initial commit

---
+++ board.h
@@ -0,0 +1,6 @@
+#ifndef KOIOS_BOARD_H
+#define KOIOS_BOARD_H
+
+
+
+#endif //KOIOS_BOARD_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "****The Koios chess engine****" << std::endl;
+    return 0;
+}
